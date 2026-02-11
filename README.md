0292c60b1cd8dddb97f4ddc5dc7607c02eeda413
xavannahb <xavannahbaars@icloud.com>
2026-02-11 17:40:00 +0100

    push these

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Voorstel Website
+# Voorstel website
+
+## Proces per dag
+
+### Dag 1 | Dinsdag 3 feb
+
+### Dag 2 | Woensdag 4 feb
+
+### Dag 3 | Donderdag 5 feb
+
+### Dag 4 | Vrijdag 6 feb
+
+### Dag 5 | Maandag 9 feb
+
+### Dag 6 | Dinsdag 10 feb
+
+### Dag 7 | Woensdag 11 feb
+
+## Eind website
--- javascript.js
+++ javascript.js
@@ -1,8 +1,7 @@
-
 // API Ophalen
 let base = "https://fdnd.directus.app/items"
 
-// API functie 4 honden liefhebbers
+// API functie 4 honden liefhebbers 
 let deLijst = document.querySelector("#dialog11 ul")
 
 getMinorMensen()
@@ -12,9 +11,7 @@ async function getMinorMensen() {
   let url = base + endpoint
     
     let response = await fetch(url)
-
     let responseJson = await response.json()
-
     let deMinorMensen = responseJson.data
 
     deMinorMensen.forEach( deMinorMens => {
@@ -25,9 +22,12 @@ async function getMinorMensen() {
             <h2>${deMinorMens.name}</h2>
             <p>Favoriete dier: ${deMinorMens.fav_animal}</p>
           </li>`
+    
     deLijst.insertAdjacentHTML("beforeend", deMinorMensHTML)
     })
 }
+// Bron: Gedaan in de les met Sanne
+
 
 // API functie Louise
 let deLijstLouise = document.querySelector("#dialog6 ul")
@@ -40,7 +40,6 @@ async function getLouise() {
 
   let response = await fetch(url)
   let responseJson = await response.json()
-
   let louise = responseJson.data 
 
   let louiseHTML =
@@ -55,18 +54,17 @@ async function getLouise() {
 
   deLijstLouise.insertAdjacentHTML("beforeend", louiseHTML)
 }
+// Bron: De namen naar Louise aangepast en de functie forEach weggelaten omdat er maar 1 persoon wordt opgehaald.
 
 // list.js search functie 
-
 var options = {
     valueNames: [ 'title', 'description' ]
 };
 
 var tegelList = new List('tegels', options);
+// Bron: https://listjs.com/examples/existing-list/
 
-
-// filteren op categorie
-
+// filteren op class 
 document.querySelectorAll('.filters button').forEach(function(button) {
     button.addEventListener('click', function() {
 
@@ -82,8 +80,9 @@ document.querySelectorAll('.filters button').forEach(function(button) {
 
     });
 });
+// Bron: https://listjs.com/examples/existing-list/ en de rest is gedaan met chatGPT als hulp prompt:'Kan je mij helpen om classes aan de li te geven om te kunnen filteren tussen de li. Dus als deze geklikt worden dat ze tonen.'
 
-
+// Bron: Gedaan in de les met Sanne
 document.querySelectorAll('.filters button').forEach(button => {
   button.addEventListener('click', function () {
 
@@ -94,3 +93,4 @@ document.querySelectorAll('.filters button').forEach(button => {
     this.classList.add('active');
   });
 });
+// Bron: ChatGPT | Dezelfde prompt als hierboven:'Kan je mij helpen om classes aan de li te geven om te kunnen filteren tussen de li. Dus als deze geklikt worden dat ze tonen.'
